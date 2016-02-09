395192812c3b4e345e504d0f4f7ede9b8e962c9a
Milco Numan <milco.numan@gmail.com>
2016-02-09 15:18:23 +0100

    Setup Django, migrated database for first time (p 530)

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local Apps
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'bloggy_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'bloggy.db'),
     }
 }
 
